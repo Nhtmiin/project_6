9eb274394680a29131574a4f932d27e1467a481e
Kien <kienntrann118@gmail.com>
2025-05-17 08:55:28 +0700

    Add code lên branch Kien

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Chia;
+using cộng;
+using Trừ;
+
+namespace Menu_1._2
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cong f = new cong();
+            f.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Tru f = new Tru();
+            f.Show();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            chia f = new chia();
+            f.Show();
+        }
+    }
+}
